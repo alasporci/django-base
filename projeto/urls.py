6312ed0d94a471b1aaceb8605dee2660955352a1
alasporci <ramanudia@gmail.com>
2022-12-11 13:26:08 -0300

    first views

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def my_views(request):
+    return HttpResponse("um string")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_views),
 ]
